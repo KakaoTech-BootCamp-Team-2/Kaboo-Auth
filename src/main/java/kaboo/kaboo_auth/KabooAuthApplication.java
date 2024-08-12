57b70656efebadf5745954ed216d93a3b5da7d28
taejin1221 <taejin7824@gmail.com>
2024-08-12 15:44:02 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/kaboo/kaboo_auth/KabooAuthApplication.java
@@ -0,0 +1,13 @@
+package kaboo.kaboo_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabooAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabooAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaboo/kaboo_auth/KabooAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package kaboo.kaboo_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabooAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
