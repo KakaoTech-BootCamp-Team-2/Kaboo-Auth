87b5a66a929599aa2b106447d1631e358a2cf151
taejin1221 <taejin7824@gmail.com>
2024-09-03 11:05:31 +0900

    Chore: Test용 application yml 추가
    Related to: #2

--- src/test/java/kaboo/kaboo_auth/KabooAuthApplicationTests.java
+++ src/test/java/kaboo/kaboo_auth/KabooAuthApplicationTests.java
@@ -2,8 +2,10 @@ package kaboo.kaboo_auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KabooAuthApplicationTests {
 
 	@Test
